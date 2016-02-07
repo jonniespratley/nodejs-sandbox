b238130ecc15f7f11f476ac53b223fea00a7c2d8
Jonnie Spratley <jonniespratley@gmail.com>
2016-02-06 21:00:54 -0800

    fix: added db

---
+++ src/plugins/db/db-spec.js
@@ -0,0 +1,9 @@
+'use strict';
+const assert = require('assert');
+const db = require('./');
+
+describe('db', function() {
+  it('should be defined', function() {
+    assert(db);
+  });
+});
---
+++ src/plugins/db/index.js
@@ -0,0 +1,11 @@
+'use strict';
+const level = require('level');
+const sublevel = require('level-sublevel');
+
+module.exports = function(dbName) {
+  return sublevel(
+    level(dbName, {
+      valueEncoding: 'json'
+    })
+  );
+};
