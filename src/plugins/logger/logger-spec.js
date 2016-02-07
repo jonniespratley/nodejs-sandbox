3447fe787b1c5de8448f2a7e5eac0af102b97516
Jonnie Spratley <jonniespratley@gmail.com>
2016-02-06 18:50:50 -0800

    fix: upated docs

--- src/plugins/logger/logger-spec.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const assert = require('assert');
-const Logger = require('./');
-
-describe('Logger', function() {
-  it('should be defined', function() {
-    assert(Logger);
-  });
-});
