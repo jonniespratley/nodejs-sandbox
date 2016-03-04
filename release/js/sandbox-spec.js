94500c7cf45da7fe3a6bc2737991cad620466d91
Jonnie Spratley <jonnie.spratley@ge.com>
2016-03-04 14:20:12 -0800

    cleaned

--- release/js/sandbox-spec.js
+++
@@ -1,3 +0,0 @@
-
-
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzYW5kYm94LXNwZWMuanMiLCJzb3VyY2VzQ29udGVudCI6W10sInNvdXJjZVJvb3QiOiIvc291cmNlLyJ9
